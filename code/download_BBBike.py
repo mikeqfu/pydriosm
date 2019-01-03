69d38ec03eb5a740bb049fabacdfeb42bda7c071
mikeqfu <qian.fu@outlook.com>
2019-01-03 22:22:17 +0000

    fixed major bugs

--- code/download_BBBike.py
+++ code/download_BBBike.py
@@ -2,15 +2,14 @@
 
 import os
 import re
-import time
-from urllib.request import urlretrieve
-from urllib.parse import urljoin
+import urllib.parse
+import urllib.request
 
+import bs4
 import fuzzywuzzy.process
 import pandas as pd
-import progressbar
 
-from utils import cd_dat, cd_dat_bbbike, save_pickle, load_pickle
+from utils import cd_dat, cd_dat_bbbike, confirmed, load_pickle, save_pickle
 
 
 #
@@ -20,18 +19,18 @@ def get_bbbike_subregion_index(url='http://download.bbbike.org/osm/bbbike/', upd
     :param update:
     :return: 
     """
-    path_to_file = cd_dat('BBBike-subregion-index.pickle')
+    path_to_file = cd_dat("BBBike-subregion-index.pickle")
     if os.path.isfile(path_to_file) and not update:
-        subregion_index = load_pickle(path_to_file)
+        bbbike_subregion_index = load_pickle(path_to_file)
     else:
         try:
-            subregion_index = pd.read_html(url, header=0, parse_dates=['Last Modified'])[0].drop(0)
-            subregion_index.Name = subregion_index.Name.map(lambda x: x.strip('/'))
-            save_pickle(subregion_index, path_to_file)
+            bbbike_subregion_index = pd.read_html(url, header=0, parse_dates=['Last Modified'])[0].drop(0)
+            bbbike_subregion_index.Name = bbbike_subregion_index.Name.map(lambda x: x.strip('/'))
+            save_pickle(bbbike_subregion_index, path_to_file)
         except Exception as e:
-            print("Getting BBBike directory index ... failed due to '{}'".format(e))
-            subregion_index = None
-    return subregion_index
+            print("Getting BBBike directory index ... Failed. \"{}\"".format(e))
+            bbbike_subregion_index = None
+    return bbbike_subregion_index
 
 
 #
@@ -42,12 +41,13 @@ def get_bbbike_subregion_downloads_index(subregion, update=False, verbose=True):
     :param verbose: [bool]
     :return: 
     """
-    subregion_name = fuzzywuzzy.process.extractOne(subregion, get_bbbike_subregion_index().Name, score_cutoff=10)[0]
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name = fuzzywuzzy.process.extractOne(subregion, bbbike_subregion_index.Name)[0]
 
     if verbose:
         if subregion != subregion_name:
-            print("'{}' is not found. \n".format(subregion))
-        print("Trying to get downloads index for '{}' ... ".format(subregion_name), end="")
+            print("\"{}\" is not found. \n".format(subregion))
+        print("Trying to get downloads index for \"{}\" ... ".format(subregion_name), end="")
 
     path_to_file = cd_dat_bbbike(subregion_name, subregion_name + "-url-index.pickle")
     if os.path.isfile(path_to_file) and not update:
@@ -56,18 +56,38 @@ def get_bbbike_subregion_downloads_index(subregion, update=False, verbose=True):
             print("Done.")
     else:
         try:
-            url = 'http://download.bbbike.org/osm/bbbike/{}/'.format(subregion_name)
-            subregion_downloads_index = pd.read_html(url)[0]
-            subregion_downloads_index.columns = ['Filename', 'FileSize']
-            subregion_downloads_index['URL'] = [urljoin(url, fname) for fname in subregion_downloads_index.Filename]
+            url = 'https://download.bbbike.org/osm/bbbike/{}/'.format(subregion_name)
 
-            # subregion_downloads_index.Filename.iloc[-1] = \
-            #     '.'.join([subregion_name, subregion_downloads_index.Filename.iloc[-1]])
+            source = urllib.request.urlopen(url)
+            source_soup = bs4.BeautifulSoup(source, 'lxml')
+            download_links_class = source_soup.find_all(name='a', attrs={'class': ['download_link', 'small']})
+
+            def parse_dlc(dlc):
+                dlc_href = dlc.get('href')  # URL
+                filename, download_url = dlc_href.strip('./'), urllib.parse.urljoin(url, dlc_href)
+                if not dlc.has_attr('title'):
+                    file_format, file_size, last_update = 'Poly', None, None
+                else:
+                    if len(dlc.contents) < 3:
+                        file_format, file_size = 'Txt', None
+                    else:
+                        file_format, file_size, _ = dlc.contents  # File type and size
+                        file_format, file_size = file_format.strip(), file_size.text
+                    last_update = pd.to_datetime(dlc.get('title'))  # Date and time
+                parsed_dat = [filename, download_url, file_format, file_size, last_update]
+                return parsed_dat
+
+            parsed_dlc = [parse_dlc(x) for x in download_links_class]
+
+            col_names = ['File_name', 'Download_URL', 'File_format', 'File_size', 'Last_update']
+            subregion_downloads_index = pd.DataFrame(parsed_dlc, columns=col_names)
+
             if verbose:
                 print("Done.")
             save_pickle(subregion_downloads_index, path_to_file)
+
         except Exception as e:
-            print("Failed due to '{}'".format(subregion_name, e))
+            print("Failed. \"{}\"".format(subregion_name, e))
             subregion_downloads_index = None
 
     return subregion_downloads_index
@@ -79,66 +99,62 @@ def get_bbbike_downloads_dictionary(update=False):
     if os.path.isfile(path_to_file) and not update:
         downloads_dictionary = load_pickle(path_to_file)
     else:
-        subregion_index = get_bbbike_subregion_index()
-        downloads_index = [get_bbbike_subregion_downloads_index(s, update, verbose=False) for s in subregion_index.Name]
-        downloads_dictionary = dict(zip(subregion_index.Name, downloads_index))
+        bbbike_subregion_index = get_bbbike_subregion_index(update=update)
+        downloads_index = [get_bbbike_subregion_downloads_index(s, update, verbose=False)
+                           for s in bbbike_subregion_index.Name]
+        downloads_dictionary = dict(zip(bbbike_subregion_index.Name, downloads_index))
         save_pickle(downloads_dictionary, path_to_file)
     return downloads_dictionary
 
 
 #
-def download_subregion_osm_file(subregion, file_format=".osm.pbf", update=False):
-
-    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update)
-    subregion_name = fuzzywuzzy.process.extractOne(subregion, get_bbbike_subregion_index().Name, score_cutoff=10)[0]
+def download_bbbike_subregion_osm_file(subregion, file_format=".osm.pbf", update=False):
+    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update=False)
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name = fuzzywuzzy.process.extractOne(subregion, bbbike_subregion_index.Name, score_cutoff=10)[0]
 
     subregion_downloads = bbbike_downloads_dict[subregion_name]
 
-    available_file_formats = [re.sub('{}|CHECKSUM'.format(subregion_name), '', f) for f in subregion_downloads.Filename]
+    available_formats = [re.sub('{}|CHECKSUM'.format(subregion_name), '', f) for f in subregion_downloads.File_name]
 
-    file_fmt, _ = fuzzywuzzy.process.extractOne(file_format, available_file_formats)
-    filename, _, idx = fuzzywuzzy.process.extractOne(file_fmt, subregion_downloads.Filename)
-    url, path_to_file = subregion_downloads.URL[idx], cd_dat_bbbike(subregion_name, filename)
+    file_fmt, _ = fuzzywuzzy.process.extractOne(file_format, available_formats)
+    filename, _, idx = fuzzywuzzy.process.extractOne(file_fmt, subregion_downloads.File_name)
+    url, path_to_file = subregion_downloads.Download_URL[idx], cd_dat_bbbike(subregion_name, filename)
 
-    # Make a custom bar to show downloading progress --------------------------
-    def make_custom_progressbar():
-        widgets = [progressbar.Bar(), ' ', progressbar.Percentage(),
-                   ' [', progressbar.Timer(), '] ',
-                   progressbar.FileTransferSpeed(),
-                   ' (', progressbar.ETA(), ') ']
-        progress_bar = progressbar.ProgressBar(widgets=widgets)
-        return progress_bar
-
-    pbar = make_custom_progressbar()
-
-    def show_progress(block_count, block_size, total_size):
-        if pbar.max_value is None:
-            pbar.max_value = total_size
-            pbar.start()
-        pbar.update(min(block_count * block_size, total_size))
-
-    try:
-        urlretrieve(url, path_to_file, reporthook=show_progress)
-        pbar.finish()
-        time.sleep(0.1)
-        print("'{}' successfully downloaded.".format(filename))
-        print("File has been saved to '{}'".format(path_to_file))
-    except Exception as e:
-        print("Downloading '{}' failed due to '{}'.".format(filename, e))
+    if os.path.isfile(path_to_file) and not update:
+        print("\"{}\" is already available for \"{}\".".format(filename, subregion_name))
+    else:
+        if confirmed(prompt="Confirm to download \"{}\"?".format(filename)):
+            try:
+                urllib.request.urlretrieve(url, path_to_file)
+                print("\"{}\" successfully downloaded.".format(filename))
+                print("File has been saved to \"{}\"".format(path_to_file))
+            except Exception as e:
+                print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
 
 
 #
-def download_subregion_osm_files(subregion, update=False):
-    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update)
-    subregion_name = fuzzywuzzy.process.extractOne(subregion, get_bbbike_subregion_index().Name, score_cutoff=10)[0]
+def download_bbbike_subregion_osm_all_files(subregion):
+    bbbike_downloads_dict = get_bbbike_downloads_dictionary(update=False)
+    bbbike_subregion_index = get_bbbike_subregion_index(update=False)
+    subregion_name = fuzzywuzzy.process.extractOne(subregion, bbbike_subregion_index.Name, score_cutoff=10)[0]
 
     subregion_downloads = bbbike_downloads_dict[subregion_name]
 
-    for url, filename in zip(subregion_downloads.URL, subregion_downloads.Filename):
-        try:
-            print("'{}' successfully downloaded.".format(filename))
-            urlretrieve(url, cd_dat_bbbike(subregion_name, filename))
-        except Exception as e:
-            print("Downloading '{}' failed due to '{}'.".format(filename, e))
-
-    print("Files saved to '{}'.".format(cd_dat_bbbike(subregion_name)))
+    if confirmed(prompt="Confirm to download all available BBBike files for \"{}\"?".format(subregion_name)):
+        for url, filename in zip(subregion_downloads.Download_URL, subregion_downloads.File_name):
+            try:
+                print("\"{}\" successfully downloaded.".format(filename))
+                urllib.request.urlretrieve(url, cd_dat_bbbike(subregion_name, filename))
+            except Exception as e:
+                print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
+        print("All the downloaded files have been saved to \"{}\".".format(cd_dat_bbbike(subregion_name)))
+    else:
+        for url, filename in zip(subregion_downloads.Download_URL, subregion_downloads.File_name):
+            if confirmed(prompt="Download \"{}\"?".format(filename)):
+                try:
+                    print("\"{}\" successfully downloaded.".format(filename))
+                    urllib.request.urlretrieve(url, cd_dat_bbbike(subregion_name, filename))
+                    print("\"{}\" saved to \"{}\".".format(filename, cd_dat_bbbike(subregion_name)))
+                except Exception as e:
+                    print("Downloading \"{}\" failed due to \"{}\".".format(filename, e))
