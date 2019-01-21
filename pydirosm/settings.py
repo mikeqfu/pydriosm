572f194dda9407c8356ed7b03e95d7931a74ab43
mikeqfu <qian.fu@outlook.com>
2019-01-21 17:01:45 +0000

    Move gdal_configurations to the front of the script

--- pydirosm/settings.py
+++ pydirosm/settings.py
@@ -7,7 +7,25 @@ import numpy.core
 import pandas
 
 
-# Set preferences for displaying results
+# Set GDAL configurations
+def gdal_configurations(reset=False):
+    if not reset:
+        # Whether to enable interleaved reading. Defaults to NO.
+        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
+        # Whether to enable custom indexing. Defaults to YES.
+        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
+        # Whether to compress nodes in temporary DB. Defaults to NO.
+        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
+        # Maximum size in MB of in-memory temporary file. If it exceeds that value, it will go to disk. Defaults to 100.
+        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '2000')
+    else:
+        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'NO')
+        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
+        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
+        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
+
+
+# Set preferences for displaying results in console
 def pd_preferences(reset=False):
     if not reset:
         pandas.set_option('expand_frame_repr', False)  # Set the representation of DataFrame NOT to wrap
@@ -21,7 +39,7 @@ def pd_preferences(reset=False):
         pandas.reset_option('all')
 
 
-# Set preferences for displaying results
+# Set preferences for displaying results in console
 def np_preferences(reset=False):
     if not reset:
         numpy.core.arrayprint._line_width = 120
@@ -57,21 +75,3 @@ def mpl_preferences(use_cambria=False, reset=False):
     else:
         matplotlib.pyplot.style.use('classic')
         matplotlib.pyplot.rcParams = matplotlib.rcParamsDefault
-
-
-# Set GDAL configurations
-def gdal_configurations(reset=False):
-    if not reset:
-        # Whether to enable interleaved reading. Defaults to NO.
-        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
-        # Whether to enable custom indexing. Defaults to YES.
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
-        # Whether to compress nodes in temporary DB. Defaults to NO.
-        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
-        # Maximum size in MB of in-memory temporary file. If it exceeds that value, it will go to disk. Defaults to 100.
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '2000')
-    else:
-        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'NO')
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
-        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
