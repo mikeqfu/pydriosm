f9bb963bd912e2b9979f23e930617ea887ceaaac
mikeqfu <qian.fu@outlook.com>
2019-01-22 22:29:47 +0000

    Remove redundant functions

--- pydirosm/settings.py
+++ pydirosm/settings.py
@@ -1,10 +1,6 @@
 """ Settings """
 
 import gdal
-import matplotlib
-import matplotlib.pyplot
-import numpy.core
-import pandas
 
 
 # Set GDAL configurations
@@ -23,55 +19,3 @@ def gdal_configurations(reset=False):
         gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
         gdal.SetConfigOption('COMPRESS_NODES', 'NO')
         gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
-
-
-# Set preferences for displaying results in console
-def pd_preferences(reset=False):
-    if not reset:
-        pandas.set_option('expand_frame_repr', False)  # Set the representation of DataFrame NOT to wrap
-        pandas.set_option('display.width', 560)  # Set the display width
-        # set_option('precision', 4)
-        pandas.set_option('display.max_columns', 100)
-        pandas.set_option('display.max_rows', 10)
-        pandas.set_option('io.excel.xlsx.writer', 'xlsxwriter')
-        pandas.set_option('mode.chained_assignment', None)
-    else:
-        pandas.reset_option('all')
-
-
-# Set preferences for displaying results in console
-def np_preferences(reset=False):
-    if not reset:
-        numpy.core.arrayprint._line_width = 120
-    else:
-        numpy.core.arrayprint._line_width = 80  # 75
-
-
-# Set preferences for plotting
-def mpl_preferences(use_cambria=False, reset=False):
-    """
-
-    # import matplotlib as mpl
-    # mpl.rc('font', family='Times New Roman')
-
-    # Get a list of supported file formats for matplotlib savefig() function
-    # plt.gcf().canvas.get_supported_filetypes()
-    # plt.gcf().canvas.get_supported_filetypes_grouped()
-    # (Aside: "gcf" is short for "get current fig" manager)
-
-    """
-    if not reset:
-        matplotlib.pyplot.style.use('ggplot')
-        if use_cambria:  # Use the font, 'Cambria'
-            # Add 'Cambria' to the front of the 'font.serif' list
-            matplotlib.pyplot.rcParams['font.serif'] = ['Cambria'] + matplotlib.pyplot.rcParams['font.serif']
-            # Set 'font.family' to 'serif', so that matplotlib will use that list
-            matplotlib.pyplot.rcParams['font.family'] = 'serif'
-        matplotlib.pyplot.rcParams['font.size'] = 13
-        matplotlib.pyplot.rcParams['font.weight'] = 'normal'
-        matplotlib.pyplot.rcParams['legend.labelspacing'] = 0.9
-        matplotlib.pyplot.rcParams['hatch.linewidth'] = 1.0
-        matplotlib.pyplot.rcParams['hatch.color'] = 'k'
-    else:
-        matplotlib.pyplot.style.use('classic')
-        matplotlib.pyplot.rcParams = matplotlib.rcParamsDefault
