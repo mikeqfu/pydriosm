35a933d56a89d57852c5246a2020190a356ec97f
mikeqfu <qian.fu@outlook.com>
2019-01-22 09:12:24 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='pydirosm',
+    version='0.0.2',
+    author='Qian Fu',
+    author_email='qian.fu@outlook.com',
+    description="A tool Download and parse OSM data extracts (Python3.x)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mikeqfu/pydirosm',
+    install_requires=[
+        'bs4',
+        'fuzzywuzzy',
+        'humanfriendly',
+        'numpy',
+        'requests',
+        'nltk',
+        'progressbar',
+        'shapely',
+        'tqdm'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+    ],
+)
