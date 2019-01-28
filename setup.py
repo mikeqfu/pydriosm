9cc57ae9fe6f25050a78745bcfda1f5b4a5cc777
mikeqfu <qian.fu@outlook.com>
2019-01-28 01:03:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as readme:
 setuptools.setup(
 
     name='pydriosm',
-    version='0.0.10',
+    version='0.1.0',
 
     author='Qian Fu',
     author_email='qian.fu@outlook.com',
