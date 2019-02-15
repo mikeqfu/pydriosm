4db7d7f49016f3e5b1e5f7301810c9e944d66d24
mikeqfu <qian.fu@outlook.com>
2019-02-15 12:49:21 +0000

    Modify variable names in scrape_region_subregion_index()

--- pydriosm/download_GeoFabrik.py
+++ pydriosm/download_GeoFabrik.py
@@ -214,16 +214,16 @@ def scrape_continent_subregion_tables():
 def scrape_region_subregion_index(subregion_tables):
 
     having_subregions = copy.deepcopy(subregion_tables)
-    region_index_updated = copy.deepcopy(subregion_tables)
+    region_subregion_index = copy.deepcopy(subregion_tables)
 
-    without_subregion = []
+    no_subregions = []
     for k, v in subregion_tables.items():
         if v is not None and isinstance(v, pd.DataFrame):
-            region_index_updated = update_nested_dict(subregion_tables, {k: set(v.Subregion)})
+            region_subregion_index = update_nested_dict(subregion_tables, {k: set(v.Subregion)})
         else:
-            without_subregion.append(k)
+            no_subregions.append(k)
 
-    for x in without_subregion:
+    for x in no_subregions:
         having_subregions.pop(x)
 
     having_subregions_temp = copy.deepcopy(having_subregions)
@@ -236,13 +236,13 @@ def scrape_region_subregion_index(subregion_tables):
             sub_subregion_tables = dict(zip(subregion_names, [get_subregion_table(link) for link in subregion_links]))
 
             subregion_index, without_subregion_ = scrape_region_subregion_index(sub_subregion_tables)
-            without_subregion += without_subregion_
+            no_subregions += without_subregion_
 
-            region_index_updated.update({region_name: subregion_index})
+            region_subregion_index.update({region_name: subregion_index})
 
             having_subregions_temp.pop(region_name)
 
-    return region_index_updated, without_subregion
+    return region_subregion_index, no_subregions
 
 
 # Get region-subregion index, or all regions having no subregions
