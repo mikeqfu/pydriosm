d85af9a3a37cbb8ffb2280090844dc61bde706f2
mikeqfu <qian.fu@outlook.com>
2019-03-11 14:59:10 +0000

    Modified settings

--- pydriosm/settings.py
+++ pydriosm/settings.py
@@ -13,7 +13,7 @@ def gdal_configurations(reset=False, max_tmpfile_size=2500):
         # Whether to enable interleaved reading. Defaults to NO.
         gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
         # Whether to enable custom indexing. Defaults to YES.
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'NO')
+        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
         # Whether to compress nodes in temporary DB. Defaults to NO.
         gdal.SetConfigOption('COMPRESS_NODES', 'YES')
         # Maximum size in MB of in-memory temporary file. If it exceeds that value, it will go to disk. Defaults to 100.
