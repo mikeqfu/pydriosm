0780de0b3332fede81fced65c6dda69bf3031fce
mikeqfu <qian.fu@outlook.com>
2019-05-24 19:09:56 +0100

    Added setting options for pandas

--- pydriosm/settings.py
+++ pydriosm/settings.py
@@ -5,6 +5,7 @@ Reference: https://www.gdal.org/drv_osm.html
 """
 
 import gdal
+import pandas
 
 
 # Set GDAL configurations
@@ -23,3 +24,19 @@ def gdal_configurations(reset=False, max_tmpfile_size=2500):
         gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
         gdal.SetConfigOption('COMPRESS_NODES', 'NO')
         gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
+
+
+# Set preferences for displaying results
+def pd_preferences(reset=False):
+    if not reset:
+        pandas.set_option('display.precision', 2)
+        pandas.set_option('expand_frame_repr', False)  # Set the representation of DataFrame NOT to wrap
+        pandas.set_option('display.width', 600)  # Set the display width
+        pandas.set_option('precision', 4)
+        pandas.set_option('display.max_columns', 100)
+        pandas.set_option('display.max_rows', 20)
+        pandas.set_option('io.excel.xlsx.writer', 'xlsxwriter')
+        pandas.set_option('mode.chained_assignment', None)
+        pandas.set_option('display.float_format', lambda x: '%.4f' % x)
+    else:
+        pandas.reset_option('all')
