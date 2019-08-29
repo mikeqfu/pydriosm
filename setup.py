c9faa653488036e43b332dc61a9e6614018f785f
mikeqfu <qian.fu@outlook.com>
2019-08-29 21:59:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("README.md", 'r') as readme:
 setuptools.setup(
 
     name='pydriosm',
-    version='1.0.14',
+    version='1.0.15',
 
     author='Qian Fu',
     author_email='qian.fu@outlook.com',
 
-    description="Download, parse and store OSM data extracts",
+    description="Download, read/parse and import/export OpenStreetMap data extracts",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
@@ -46,15 +46,10 @@ setuptools.setup(
 
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Operating System :: Microsoft :: Windows :: Windows 7',
-        'Operating System :: Microsoft :: Windows :: Windows 8',
-        'Operating System :: Microsoft :: Windows :: Windows 8.1',
-        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Operating System :: Microsoft :: Windows',
     ],
 )
 
