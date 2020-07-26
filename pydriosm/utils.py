c5ab781ce15654037f749d2268a0d45d9c3e4f50
mikeqfu <qian.fu@outlook.com>
2020-07-26 17:11:24 +0100

    modified utils.py

--- pydriosm/utils.py
+++ pydriosm/utils.py
@@ -1,101 +1,104 @@
 """ Utilities - Helper functions """
 
 import os
-import pickle
 
 import pkg_resources
-import pyhelpers.dir
-import rapidjson
-
-# ====================================================================================================================
-""" Change directory """
+from pyhelpers.dir import cd
 
 
-# Change directory to "dat_GeoFabrik" and sub-directories
-def cd_dat_geofabrik(*sub_dir):
-    path = pyhelpers.dir.cd("dat_GeoFabrik")
+# -- Change directory ---------------------------------------------------------------------------------
+
+def cd_dat(*sub_dir, dat_dir="dat", mkdir=False, **kwargs):
+    """
+    Change directory to ``dat_dir`` and its sub-directories within a package.
+
+    :param sub_dir: name of directory; names of directories (and/or a filename)
+    :type sub_dir: str
+    :param dat_dir: name of a directory to store data, defaults to ``"dat"``
+    :type dat_dir: str
+    :param mkdir: whether to create a directory, defaults to ``False``
+    :type mkdir: bool
+    :param kwargs: optional parameters of `os.makedirs`_, e.g. ``mode=0o777``
+    :return: a full path to a directory (or a file) under ``data_dir``
+    :rtype: str
+
+    .. _`os.makedirs`: https://docs.python.org/3/library/os.html#os.makedirs
+
+    **Example**::
+
+        from pyrcs.utils import cd_dat
+
+        dat_dir = "dat"
+        mkdir = False
+
+        cd_dat("line-data", dat_dir=dat_dir, mkdir=mkdir)
+        # "\\dat\\line-data"
+    """
+
+    path = pkg_resources.resource_filename(__name__, dat_dir)
+
     for x in sub_dir:
         path = os.path.join(path, x)
+
+    if mkdir:
+        path_to_file, ext = os.path.splitext(path)
+
+        if ext == '':
+            os.makedirs(path_to_file, exist_ok=True, **kwargs)
+        else:
+            os.makedirs(os.path.dirname(path_to_file), exist_ok=True, **kwargs)
+
     return path
 
 
-# Change directory to "dat_BBBike" and sub-directories
-def cd_dat_bbbike(*sub_dir):
-    path = pyhelpers.dir.cd("dat_BBBike")
-    for x in sub_dir:
-        path = os.path.join(path, x)
+def cd_dat_geofabrik(*sub_dir, mkdir=False, **kwargs):
+    """
+    Change directory to ``dat_GeoFabrik`` and its sub-directories within a package.
+
+    :param sub_dir: name of directory; names of directories (and/or a filename)
+    :type sub_dir: str
+    :param mkdir: whether to create a directory, defaults to ``False``
+    :type mkdir: bool
+    :param kwargs: optional parameters of `os.makedirs`_, e.g. ``mode=0o777``
+    :return: a full path to a directory (or a file) under ``data_dir``
+    :rtype: str
+    """
+
+    path = cd("dat_GeoFabrik", *sub_dir, mkdir=mkdir, **kwargs)
+
     return path
 
 
-# Change directory to "dat" and sub-directories
-def cd_dat(*sub_dir):
-    path = pkg_resources.resource_filename(__name__, 'dat/')
-    for x in sub_dir:
-        path = os.path.join(path, x)
+def cd_dat_bbbike(*sub_dir, mkdir=False, **kwargs):
+    """
+    Change directory to ``dat_BBBike`` and its sub-directories.
+
+    :param sub_dir: name of directory; names of directories (and/or a filename)
+    :type sub_dir: str
+    :param mkdir: whether to create a directory, defaults to ``False``
+    :type mkdir: bool
+    :param kwargs: optional parameters of `os.makedirs`_, e.g. ``mode=0o777``
+    :return: a full path to a directory (or a file) under ``data_dir``
+    :rtype: str
+    """
+
+    path = cd("dat_BBBike", *sub_dir, mkdir=mkdir, **kwargs)
+
     return path
 
 
-# ====================================================================================================================
-""" Save data """
+# -- Miscellaneous ------------------------------------------------------------------------------------
 
-
-# Save Pickle file
-def save_pickle(pickle_data, path_to_pickle, verbose=True):
-    """
-    :param pickle_data: any object that could be dumped by the 'pickle' package
-    :param path_to_pickle: [str] local file path
-    :param verbose: [bool] (default: True)
-    :return: whether the data has been successfully saved
-    """
-    pickle_filename = os.path.basename(path_to_pickle)
-    pickle_dir = os.path.basename(os.path.dirname(path_to_pickle))
-    pickle_dir_parent = os.path.basename(os.path.dirname(os.path.dirname(path_to_pickle)))
-
-    if verbose:
-        print("{} \"{}\" ... ".format("Updating" if os.path.isfile(path_to_pickle) else "Saving",
-                                      " - ".join([pickle_dir_parent, pickle_dir, pickle_filename])), end="")
-
-    try:
-        os.makedirs(os.path.dirname(os.path.abspath(path_to_pickle)), exist_ok=True)
-        pickle_out = open(path_to_pickle, 'wb')
-        pickle.dump(pickle_data, pickle_out)
-        pickle_out.close()
-        print("Successfully.") if verbose else None
-    except Exception as e:
-        print("Failed. {}.".format(e))
-
-
-# Save JSON file
-def save_json(json_data, path_to_json, verbose=True):
-    """
-    :param json_data: any object that could be dumped by the 'json' package
-    :param path_to_json: [str] local file path
-    :param verbose: [bool] (default: True)
-    :return: whether the data has been successfully saved
-    """
-    json_filename = os.path.basename(path_to_json)
-    json_dir = os.path.basename(os.path.dirname(path_to_json))
-    json_dir_parent = os.path.basename(os.path.dirname(os.path.dirname(path_to_json)))
-
-    print("{} \"{}\" ... ".format("Updating" if os.path.isfile(path_to_json) else "Saving",
-                                  " - ".join([json_dir_parent, json_dir, json_filename])), end="") if verbose else None
-    try:
-        os.makedirs(os.path.dirname(os.path.abspath(path_to_json)), exist_ok=True)
-        json_out = open(path_to_json, 'w')
-        rapidjson.dump(json_data, json_out)
-        json_out.close()
-        print("Successfully.") if verbose else None
-    except Exception as e:
-        print("Failed. {}.".format(e))
-
-
-# ====================================================================================================================
-""" Misc """
-
-
-# Make a dictionary with keys and values being shape_type (in OSM .shp file) and shapely.geometry, respectively
 def osm_geom_types():
+    """
+    Make a dictionary for OSM geometry types.
+
+    :return: a dictionary with keys and values being shape type (in OSM .shp file) and shapely.geometry
+    :rtype: dict
+    """
+
     import shapely.geometry
+
     shape_types = {'Point': shapely.geometry.Point,
                    'LineString': shapely.geometry.LineString,
                    'LinearRing': shapely.geometry.LinearRing,
@@ -103,4 +106,5 @@ def osm_geom_types():
                    'Polygon': shapely.geometry.Polygon,
                    'MultiPolygon': shapely.geometry.MultiPolygon,
                    'GeometryCollection': shapely.geometry.GeometryCollection}
+
     return shape_types
