2aadabe66039b31391679b68c1472cc07f2c59aa
mikeqfu <qian.fu@outlook.com>
2020-08-31 17:25:37 +0100

    modified settings.py

--- pydriosm/settings.py
+++ pydriosm/settings.py
@@ -1,7 +1,6 @@
 """ Settings """
 
 import gdal
-import pandas
 
 
 def gdal_configurations(reset=False, max_tmpfile_size=5000):
@@ -29,25 +28,3 @@ def gdal_configurations(reset=False, max_tmpfile_size=5000):
         gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
         gdal.SetConfigOption('COMPRESS_NODES', 'NO')
         gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
-
-
-def pd_preferences(reset=False):
-    """
-    Set preferences for displaying results.
-
-    :param reset: reset to default settings, defaults to ``False``
-    :type reset: bool
-    """
-
-    if not reset:
-        pandas.set_option('display.precision', 2)
-        pandas.set_option('expand_frame_repr', False)  # Set the representation of DataFrame NOT to wrap
-        pandas.set_option('display.width', 600)  # Set the display width
-        pandas.set_option('precision', 4)
-        pandas.set_option('display.max_columns', 100)
-        pandas.set_option('display.max_rows', 20)
-        pandas.set_option('io.excel.xlsx.writer', 'xlsxwriter')
-        pandas.set_option('mode.chained_assignment', None)
-        pandas.set_option('display.float_format', lambda x: '%.4f' % x)
-    else:
-        pandas.reset_option('all')
