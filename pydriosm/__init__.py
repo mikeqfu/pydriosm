a46eb07a5924891f1b52e7f8cbbd194f9e63a1b3
mikeqfu <qian.fu@outlook.com>
2020-11-18 11:32:36 +0000

    modify __init__.py

--- pydriosm/__init__.py
+++ pydriosm/__init__.py
@@ -14,5 +14,4 @@ __package_name_alt__ = u'PyDriosm'
 __version__ = '2.0.0'
 __author__ = u'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
-__description__ = \
-    "A handy tool for downloading and reading OSM data and its I/O with PostgreSQL."
+__description__ = "An open-source tool for downloading, reading and basic PostgreSQL I/O of OpenStreetMap data."
