d7cb423ae30dc3443139fc6063ea3ce24ed7afd9
mikeqfu <qian.fu@outlook.com>
2020-11-24 00:11:26 +0000

    update __init__.py

--- pydriosm/__init__.py
+++ pydriosm/__init__.py
@@ -1,9 +1,6 @@
 from .downloader import BBBikeDownloader, GeofabrikDownloader
 from .ios import PostgresOSM
 from .reader import BBBikeReader, GeofabrikReader
-from .settings import gdal_configurations
-
-gdal_configurations(reset=False)
 
 __all__ = ['downloader', 'GeofabrikDownloader', 'BBBikeDownloader',
            'reader', 'GeofabrikReader', 'BBBikeReader',
@@ -11,7 +8,7 @@ __all__ = ['downloader', 'GeofabrikDownloader', 'BBBikeDownloader',
 
 __package_name__ = 'pydriosm'
 __package_name_alt__ = 'PyDriosm'
-__version__ = '2.0.1'
-__author__ = 'Qian Fu'
+__version__ = '2.0.2'
+__author__ = u'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
-__description__ = "An open-source tool for downloading, reading and PostgreSQL-based I/O of OpenStreetMap data."
+__description__ = 'An open-source tool for downloading, reading and PostgreSQL-based I/O of OpenStreetMap data.'
