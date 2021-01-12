05c9912113802f9818618a18967be22cdb60ac50
mikeqfu <qian.fu@outlook.com>
2021-01-12 23:04:14 +0000

    Modify setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ with open("README.rst", 'r', encoding='utf-8') as readme:
 setuptools.setup(
 
     name=pydriosm.__package_name__,
+
     version=pydriosm.__version__,
-    author=pydriosm.__author__,
-    author_email=pydriosm.__email__,
 
     description=pydriosm.__description__,
     long_description=long_description,
@@ -18,36 +17,65 @@ setuptools.setup(
 
     url='https://github.com/mikeqfu/pydriosm',
 
+    author=pydriosm.__author__,
+    author_email=pydriosm.__email__,
+
+    license='GPLv3',
+
+    classifiers=[
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'
+    ],
+
+    keywords=['Python',
+              'OpenStreetMap', 'OSM', 'PostgreSQL',
+              'Geofabrik', 'BBBike',
+              'Protocolbuffer Binary Format', 'PBF Format',
+              'Shapefile', 'Shapefiles'],
+
+    project_urls={
+        'Documentation': 'https://pydriosm.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/mikeqfu/pydriosm',
+        'Tracker': 'https://github.com/mikeqfu/pydriosm/issues',
+    },
+
+    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
+
     install_requires=[
-        'beautifulsoup4~=4.9.3',
-        'Fiona~=1.8.17',
+        'beautifulsoup4',
+        'Fiona',
         'fuzzywuzzy',
         'GDAL~=3.1.4',
-        'geopandas~=0.8.1',
+        'geopandas',
         'html5lib',
-        'humanfriendly~=8.2',
+        'humanfriendly',
         'lxml',
         'more-itertools',
-        'pandas~=1.1.4',
+        'pandas~=1.1.5',
         'psycopg2',
-        'pyhelpers>=1.2.7',
+        'pyhelpers>=1.2.9',
         'pyproj',
         'pyshp',
         'requests',
-        'Shapely~=1.7.1',
+        'Shapely',
         'SQLAlchemy',
         'SQLAlchemy-Utils',
         'tqdm',
     ],
 
-    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
-
+    package_data={"": ["requirements.txt", "LICENSE"]},
     include_package_data=True,
 
-    classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux'
-    ],
 )
