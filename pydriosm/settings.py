b16071d9bbfe5db07362b550b106118f14232236
mikeqfu <qian.fu@outlook.com>
2021-04-18 23:39:11 +0100

    Modify imports in settings.py

--- pydriosm/settings.py
+++ pydriosm/settings.py
@@ -20,20 +20,20 @@ def gdal_configurations(reset=False, max_tmpfile_size=5000):
         >>> gdal_configurations()
     """
 
-    import gdal
+    import osgeo.gdal
 
     if not reset:
         # Whether to enable interleaved reading. Defaults to NO.
-        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
+        osgeo.gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'YES')
         # Whether to enable custom indexing. Defaults to YES.
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
+        osgeo.gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
         # Whether to compress nodes in temporary DB. Defaults to NO.
-        gdal.SetConfigOption('COMPRESS_NODES', 'YES')
+        osgeo.gdal.SetConfigOption('COMPRESS_NODES', 'YES')
         # Maximum size in MB of in-memory temporary file. If it exceeds that value,
         # it will go to disk. Defaults to 100.
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', str(max_tmpfile_size))
+        osgeo.gdal.SetConfigOption('MAX_TMPFILE_SIZE', str(max_tmpfile_size))
     else:
-        gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'NO')
-        gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
-        gdal.SetConfigOption('COMPRESS_NODES', 'NO')
-        gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
+        osgeo.gdal.SetConfigOption('OGR_INTERLEAVED_READING', 'NO')
+        osgeo.gdal.SetConfigOption('USE_CUSTOM_INDEXING', 'YES')
+        osgeo.gdal.SetConfigOption('COMPRESS_NODES', 'NO')
+        osgeo.gdal.SetConfigOption('MAX_TMPFILE_SIZE', '100')
