a5956a4cbe07252ead16a7def977c8362a763687
mikeqfu <qian.fu@outlook.com>
2022-05-17 19:22:13 +0100

    Modify __init__.py

--- pydriosm/__init__.py
+++ pydriosm/__init__.py
@@ -1,25 +1,40 @@
+"""
+Package initialization.
+"""
+
 import datetime
+import json
+
+import pkg_resources
 
 from .downloader import BBBikeDownloader, GeofabrikDownloader
 from .ios import PostgresOSM
 from .reader import BBBikeReader, GeofabrikReader
 
+with open(pkg_resources.resource_filename(__name__, "data/metadata.json"), mode='r') as metadata_file:
+    metadata = json.load(metadata_file)
+
+__project__ = metadata['Project']
+__pkgname__ = metadata['Package']
+
+__author__ = metadata['Author']
+__affiliation__ = metadata['Affiliation']
+__author_email__ = metadata['Email']
+
+__description__ = metadata['Description']
+
+__copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
+
+__version__ = metadata['Version']
+__license__ = metadata['License']
+
 __all__ = [
+    __author__,
+    __description__,
+    __author_email__,
+    __pkgname__,
+    __version__,
     'downloader', 'GeofabrikDownloader', 'BBBikeDownloader',
     'reader', 'GeofabrikReader', 'BBBikeReader',
     'ios', 'PostgresOSM',
 ]
-
-__project_name__ = 'PyDriosm'
-__package_name__ = 'pydriosm'
-__description__ = \
-    f'{__package_name__}: ' \
-    f'an open-source tool for downloading, reading and PostgreSQL-based I/O of OpenStreetMap data.'
-
-__author__ = 'Qian Fu'
-__affiliation__ = 'School of Engineering, University of Birmingham'
-__email__ = 'q.fu@bham.ac.uk'
-
-__copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
-
-__version__ = '2.0.4'
