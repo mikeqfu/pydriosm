b6a00d56b951542efe812412dc747ad9d1da811d
mikeqfu <qian.fu@outlook.com>
2022-05-17 11:48:49 +0100

    Modify setup.*

--- setup.py
+++ setup.py
@@ -1,82 +1,25 @@
+import json
+
 import setuptools
 
-# noinspection PyProtectedMember
-from pydriosm import __author__, __description__, __email__, __package__, __version__
+with open(file="pydriosm/data/metadata.json", mode='r') as metadata_file:
+    metadata = json.load(metadata_file)
 
-with open("README.rst", 'r', encoding='utf-8') as readme:
-    long_description = readme.read()
+__pkgname__, __version__ = metadata['Package'], metadata['Version']
+
+__home_page__ = 'https://github.com/mikeqfu/' + f'{__pkgname__}'
 
 setuptools.setup(
-
-    name=__package__,
-
+    name=__pkgname__,
     version=__version__,
-
-    description=__description__,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-
-    url='https://github.com/mikeqfu/pydriosm',
-
-    author=__author__,
-    author_email=__email__,
-
-    license='GPLv3',
-
-    classifiers=[
-        'Intended Audience :: Education',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Utilities',
-
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
-        'Programming Language :: Python :: 3',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux'
-    ],
-
-    keywords=['Python',
-              'OpenStreetMap', 'OSM', 'PostgreSQL',
-              'Geofabrik', 'BBBike',
-              'Protocolbuffer Binary Format', 'PBF Format',
-              'Shapefile', 'Shapefiles'],
-
+    description=metadata['Description'],
+    url=__home_page__,
+    author=metadata['Author'],
+    author_email=metadata['Email'],
+    license=metadata['License'],
     project_urls={
-        'Documentation': 'https://pydriosm.readthedocs.io/en/{}/'.format(__version__),
-        'Source': 'https://github.com/mikeqfu/pydriosm',
-        'Tracker': 'https://github.com/mikeqfu/pydriosm/issues',
+        'Documentation': f'https://{__pkgname__}.readthedocs.io/en/{__version__}/',
+        'Source': __home_page__,
+        'Bug Tracker': __home_page__ + '/issues',
     },
-
-    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
-
-    install_requires=[
-        'beautifulsoup4',
-        'lxml',
-        'GDAL>=3.0',
-        'html5lib',
-        'humanfriendly',
-        'lxml',
-        'more-itertools',
-        'pyshp',
-        'tqdm',
-        # 'Fiona',
-        # 'geopandas',
-        'pyhelpers>=1.2.17',  # which requires the following dependencies:
-        # 'Shapely',
-        # 'pyproj',
-        # 'requests',
-        # 'pandas',
-        # 'fuzzywuzzy',
-        # 'psycopg2',
-        # 'SQLAlchemy',
-    ],
-
-    package_data={"": ["requirements.txt", "LICENSE"]},
-    include_package_data=True,
-
 )
