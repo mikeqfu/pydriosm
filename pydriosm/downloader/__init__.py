1d73eecdb371c50e19c0fd75d0e6668d668a866e
Qian Fu <q.fu@bham.ac.uk>
2025-03-23 19:07:40 +0000

    Update `__init__`

--- pydriosm/downloader/__init__.py
+++ pydriosm/downloader/__init__.py
@@ -3,8 +3,8 @@ Download `OpenStreetMap <https://www.openstreetmap.org/>`_ (OSM) data from free 
 `Geofabrik <https://download.geofabrik.de/>`_ and `BBBike <https://download.bbbike.org/>`_.
 """
 
-from .bbbike import BBBikeDownloader
-from .geofabrik import GeofabrikDownloader
+from ._bbbike import BBBikeDownloader
+from ._geofabrik import GeofabrikDownloader
 
 __all__ = [
     'GeofabrikDownloader',
