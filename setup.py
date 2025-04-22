b55a0e44eee6c0a7ec0bbf70a02626c0ed4e985f
Qian Fu <q.fu@bham.ac.uk>
2025-04-23 00:11:51 +0100

    Modify packaging configuration

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ import setuptools
 with open(file="pydriosm/data/.metadata", mode='r') as metadata_file:
     metadata = json.load(metadata_file)
 
-__pkgname__, __version__ = metadata['Package'], metadata['Version']
-
-__home_page__ = f'https://github.com/mikeqfu/{__pkgname__}'
+__pkgname__ = metadata['Package']
+__version__ = metadata['Version']
+__homepage__ = f'https://github.com/mikeqfu/{__pkgname__}'
 
 setuptools.setup(
     name=__pkgname__,
     version=__version__,
     description=metadata['Description'],
-    url=__home_page__,
+    url=__homepage__,
     author=metadata['Author'],
     author_email=metadata['Email'],
     license=metadata['License'],
     project_urls={
         'Documentation': f'https://{__pkgname__}.readthedocs.io/en/{__version__}/',
-        'Source': __home_page__,
-        'Issue Tracker': f'{__home_page__}/issues',
+        'Source': __homepage__,
+        'Issue Tracker': f'{__homepage__}/issues',
     },
 )
