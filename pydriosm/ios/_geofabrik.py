2096f0bd18b0bd27fab17da45740b14b9737eefb
Qian Fu <q.fu@bham.ac.uk>
2026-03-05 00:31:49 +0000

    refactor(`ios`): rename geofabrik.py to _geofabrik.py and restructure code

--- pydriosm/ios/_geofabrik.py
+++ pydriosm/ios/_geofabrik.py
@@ -1,16 +1,19 @@
 """
-Implement storage I/O of (parsed) OSM data extracts (available from Geofabrik free download server)
+Implement storage I/O of OSM data extracts (available from Geofabrik free download server)
 with `PostgreSQL <https://www.postgresql.org/>`_.
 """
 
-from pydriosm.ios._ios import PostgresOSM
+from pydriosm.ios._pgsql_osm import PostgresOSM
 
 
-class GeofabrikIOS:
+class GeofabrikIOS(PostgresOSM):
     """
     Implement storage I/O of `Geofabrik OpenStreetMap data extracts <https://download.geofabrik.de/>`_
     with `PostgreSQL <https://www.postgresql.org/>`_.
     """
+
+    #: Data source.
+    DATA_SOURCES: list = ['Geofabrik']
 
     def __init__(self, **kwargs):
         """
@@ -25,22 +28,19 @@ class GeofabrikIOS:
         **Examples**::
 
             >>> from pydriosm.ios import GeofabrikIOS
-
-            >>> gfi = GeofabrikIOS(database_name='osmdb_test')
+            >>> gfi = GeofabrikIOS(database_name='osmdb_test', verbose=True)
             Password (postgres@localhost:5432): ***
             Creating a database: "osmdb_test" ... Done.
             Connecting postgres:***@localhost:5432/osmdb_test ... Successfully.
-
-            >>> type(gfi.dbms)
-            pydriosm.ios.PostgresOSM
-
-            >>> gfi.dbms.name
-            'Geofabrik OpenStreetMap data extracts'
+            >>> type(gfi.downloader)
+            pydriosm.downloader._wrapper.Downloader
+            >>> type(gfi.reader)
+            pydriosm.reader._wrapper.Reader
 
         .. seealso::
 
             - Examples for all the methods of the class :class:`~pydriosm.ios.PostgresOSM`.
         """
 
-        kwargs.update({'data_source': 'Geofabrik'})
-        self.dbms = PostgresOSM(**kwargs)
+        kwargs['data_source'] = self.DATA_SOURCES[0]
+        super().__init__(**kwargs)
