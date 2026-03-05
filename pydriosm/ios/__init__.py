1818480afa392aef6a48eea169c4de6d35a1a535
Qian Fu <q.fu@bham.ac.uk>
2026-03-05 00:33:03 +0000

    refactor(`ios`): modify __init__.py

--- pydriosm/ios/__init__.py
+++ pydriosm/ios/__init__.py
@@ -2,8 +2,9 @@
 Implement storage I/O of (parsed) OSM data extracts with `PostgreSQL <https://www.postgresql.org/>`_.
 """
 
-from ._ios import PostgresOSM
-from .bbbike import BBBikeIOS
-from .geofabrik import GeofabrikIOS
+from . import _base, utils
+from ._bbbike import BBBikeIOS
+from ._geofabrik import GeofabrikIOS
+from ._pgsql_osm import PostgresOSM
 
 __all__ = ['PostgresOSM', 'GeofabrikIOS', 'BBBikeIOS']
