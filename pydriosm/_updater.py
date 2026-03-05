0302537edd03c3aa6058bd43f29597f2e1058370
Qian Fu <q.fu@bham.ac.uk>
2026-03-05 16:51:55 +0000

    refactor(`_updater`): modify `_update_prepacked_data()`

--- pydriosm/_updater.py
+++ pydriosm/_updater.py
@@ -61,13 +61,13 @@ def _update_prepacked_data(verbose=True, interval=5):
         # -- BBBike --------------------------------------------------------------------------------
         bbd = BBBikeDownloader()
 
-        _ = bbd.get_bbbike_cities(**meth_args)
-
-        time.sleep(interval)
-
-        _ = bbd.get_coordinates_of_cities(**meth_args)
-
-        time.sleep(interval)
+        # _ = bbd.get_bbbike_cities(**meth_args)
+        #
+        # time.sleep(interval)
+        #
+        # _ = bbd.get_coordinates_of_cities(**meth_args)
+        #
+        # time.sleep(interval)
 
         _ = bbd.get_subregion_index(**meth_args)
 
